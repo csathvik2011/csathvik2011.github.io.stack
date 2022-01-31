076b42d5836282e0f3b5dff0b8124e6ddc71cf13
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-01-31 07:21:28 -0500

    Create script.js

---
+++ script.js/script.js
@@ -0,0 +1,6 @@
+for(var i=25;i>0,i--){
+let slider = document.createElement("div");
+slider.setAttribute("class","slider animate");
+slider.setAttribute("id", "slider"+1);
+document.gameElement("game").append(slider);
+}
